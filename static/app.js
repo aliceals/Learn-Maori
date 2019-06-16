641c9d4589cd96e3e3195ed55c42c63575406fba
Alice Alsford <alice.alsford@gmail.com>
2019-06-16 20:55:50 +1200

    got button working with random words

--- static/app.js
+++ static/app.js
@@ -2,9 +2,15 @@
 const maoriWord = document.getElementById("maoriWord");
 const englishWord = document.getElementById("englishWord");
 const border = document.getElementById("border");
+const btn = document.getElementById("btn");
 
-//Call backend API for maori word
-maoriWord.addEventListener("click", () => {
+console.log(btn);
+
+window.onload = function() {
+  const btn = document.getElementById("btn");
+
+  console.log(btn);
+
   fetch("/API/maoritranslations")
     .then(response => {
       return response.json();
@@ -12,15 +18,36 @@ maoriWord.addEventListener("click", () => {
     .then(json => {
       console.log(json);
     });
-});
 
-fetch("/API/maoritranslations")
-  .then(response => {
-    return response.json();
-  })
-  .then(json => {
-    var jsonMaori = json.maoriWord;
-    maoriWord.innerHTML = jsonMaori;
-    var jsonEnglish = json.englishWord;
-    englishWord.innerHTML = jsonEnglish;
+  fetch("/API/maoritranslations")
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      var jsonMaori = json.maoriWord;
+      maoriWord.innerHTML = jsonMaori;
+      var jsonEnglish = json.englishWord;
+      englishWord.innerHTML = jsonEnglish;
+    });
+
+  btn.addEventListener("click", () => {
+    fetch("/API/maoritranslations")
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+      });
+
+    fetch("/API/maoritranslations")
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        var jsonMaori = json.maoriWord;
+        maoriWord.innerHTML = jsonMaori;
+        var jsonEnglish = json.englishWord;
+        englishWord.innerHTML = jsonEnglish;
+      });
   });
+};
